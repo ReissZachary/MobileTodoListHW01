db92d043673a81b848788b87cf0228bf60706ff7
Zachary Reiss <zachary.reiss123@gmail.com>
2020-01-22 16:22:13 -0700

    Add NUnit test project

---
+++ TodoListHW01.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TodoListHW01.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
